b7831c06fa903b77351b2c6d6b03e67f4accd99f
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2020-05-04 09:51:48 -0300

    Data Transfer Object e o type utility Omit

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -3,8 +3,14 @@ import { uuid } from 'uuidv4';
 class Appointment {
   id: string;
 
-  constructor(public provider: string, public date: Date) {
+  provider: string;
+
+  date: Date;
+
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
     this.id = uuid();
+    this.provider = provider;
+    this.date = date;
   }
 }
 
